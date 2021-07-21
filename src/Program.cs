13aaaf0642d63bad6bfb2f55a5118a6e9c74a895
Beni Reydman <benireydman901@hotmail.com>
2021-07-21 13:35:02 -0500

    Read password in

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace TwitchBot
     {
         static void Main(string[] args)
         {
-            IrcClient client = new IrcClient("irc.twitch.tv", 6667, "NinjaMonkersBot", "", "joebruin");
+            IrcClient client = new IrcClient("irc.twitch.tv", 6667, "NinjaMonkersBot", System.IO.File.ReadAllText(@"Password.txt"), "ninjamonkers");
 
             var pinger = new Pinger(client);
             pinger.Start();
