2d239dcbe4c71c2dbb34ccfbff9053df47aa16ba
Beni Reydman <benireydman901@hotmail.com>
2021-07-20 23:47:43 -0500

    Initial Working Bot

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IrcClient client = new IrcClient("irc.twitch.tv", 6667, "NinjaMonkersBot", "", "joebruin");
+
+            var pinger = new Pinger(client);
+            pinger.Start();
+
+            client.SendChatMessage("Guess who's back Joe >:)");
+
+            while (true)
+            {
+                Console.WriteLine("Reading message");
+                var message = client.ReadMessage();
+                Console.WriteLine($"Message: {message}");
+            }
+        }
+    }
+}
